70c41de703b197da76a98fa9c4b1309db3a61762
Octo1996 <Octo1996@outlook.com>
2021-02-25 16:12:41 +0800

    chore(init): add playground

---
+++ packages/playground/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hello word</div>
+</template>
+<style>
+.content {
+  width: 1366px;
+  height: 768px;
+  background-color: red;
+}
+</style>
+<script>
+export default {
+  name: 'App'
+}
+</script>
